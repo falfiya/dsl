d0168e4ee79d16dc8a47bbe7b6b26a78bb562c2a
falfiya <22552895+falfiya@users.noreply.github.com>
2025-06-08 17:37:02 -0700

    SL1 PROVEN!
    heck yeah!

--- dsl/Links.lean
+++ dsl/Links.lean
@@ -1,15 +1,13 @@
 import dsl.Runtime
-import dsl.Stream
 
 namespace Synchronous
 
-namespace FairLossLink
-   structure Deliver {Message : Type} where
-      src : Process
-      dest : Process
-      msg : Message
-      sentAt : Time
-end FairLossLink
+structure FairLossLink.Deliver {Message : Type} where
+   src : Process
+   dest : Process
+   msg : Message
+   sentAt : Time
+
 structure FairLossLink where
    send : {Message : Type}
       -> (src : Process)
@@ -23,7 +21,64 @@ structure FairLossLink where
       -> (src : Process)
       -> (dest : Process)
       -> (msg : Message)
-      -> Stream' (Option {d : FairLossLink.Deliver // d.src = p ∧ d.dest = dest ∧ d.msg = msg})
+      -> (sendAt : Time)
+      -> Stream' (Option {d : FairLossLink.Deliver // d.src = src ∧ d.dest = dest ∧ d.msg = msg ∧ d.sentAt ≥ sendAt})
+      -> Stream' {d : FairLossLink.Deliver // d.src = src ∧ d.dest = dest ∧ d.msg = msg ∧ d.sentAt ≥ sendAt}
+   eventualDelivery : {Message : Type}
+      -> (src : Process)
+      -> (dest : Process)
+      -> (msg : Message)
+      -> (sendAt : Time)
+      -> Stream' (Option {d : FairLossLink.Deliver // d.src = src ∧ d.dest = dest ∧ d.msg = msg ∧ d.sentAt ≥ sendAt})
       -> ∃t, send src dest msg t = some ⟨⟨src, dest, msg, t⟩, by rfl⟩
+   finiteDuplication : NoProof
+   noCreation : NoProof
+
+
+structure StubbornLink.Deliver {Message : Type} where
+   src : Process
+   dest : Process
+   msg : Message
+   sentAt : Time
+
+structure StubbornLink where
+   fll : FairLossLink
+   -- This is also stubborn delivery
+   send : {Message : Type}
+      -> (src : Process)
+      -> (dest : Process)
+      -> (msg : Message)
+      -> (sendAt : Time)
+      -> Stream' {d : StubbornLink.Deliver // d.src = src ∧ d.dest = dest ∧ d.msg = msg ∧ d.sentAt ≥ sendAt}
+   noCreation : NoProof
+
+def StubbornLink.fromFLL (fll : FairLossLink) : StubbornLink := {
+   fll := fll,
+   send
+      {Message : Type} (src : Process) (dest : Process) (msg : Message) (sendAt : Time)
+      : Stream' {d : StubbornLink.Deliver // d.src = src ∧ d.dest = dest ∧ d.msg = msg ∧ d.sentAt ≥ sendAt}
+      := by
+         let maybeDelivered : Stream' (Option {d : FairLossLink.Deliver // d.src = src ∧ d.dest = dest ∧ d.msg = msg ∧ d.sentAt ≥ sendAt}) := (Time.all.add sendAt).map (fun ⟨sendAt', p_sendAt'⟩ => (fll.send src dest msg sendAt').map (by
+            intro ⟨delivery, p_delivery⟩
+            constructor
+            case val => exact delivery
+            simp
+            refine ⟨?_, ?_, ?_, ?_⟩
+            · rw [p_delivery]
+            · rw [p_delivery]
+            · rw [p_delivery]
+            · rw [p_delivery]; simp; exact p_sendAt'
+         ))
+         let fairLoss := fll.fairLoss src dest msg sendAt maybeDelivered;
+         exact fairLoss.map (by
+            intro ⟨fld, ⟨p_fldSrc, p_fldDest, p_fldMsg, p_fldSentAt⟩⟩
+            constructor
+            -- first construct Stubborn Delivery from Fair Loss Delivery
+            case val => exact ⟨fld.src, fld.dest, fld.msg, fld.sentAt⟩
+            simp
+            exact ⟨p_fldSrc, p_fldDest, p_fldMsg, p_fldSentAt⟩
+         )
+   noCreation := default
+}
 
 end Synchronous
--- dsl/Runtime.lean
+++ dsl/Runtime.lean
@@ -1,6 +1,34 @@
 import Mathlib.Data.Nat.Basic
 
+import Mathlib.Data.Stream.Init
+
+structure FiniteStream (α : Type) where
+   underlaying : Stream' α
+   start : Nat
+   length : Nat
+
+def FiniteStream.toList {α : Type} (fs : FiniteStream α) : List α :=
+   (List.range fs.length).map fun n => fs.underlaying (fs.start + n)
+
+def Stream'.slice (start length : Nat) (s : Stream' α) : FiniteStream α :=
+   ⟨s, start, length⟩
+
 abbrev Time := Nat
+def Time.Stream : Type := Stream' Time
+def Time.all : Stream := fun n => n
+
+instance Time.inhabited : Inhabited Time := Inhabited.mk 0
+instance Time.Stream.inhabited : Inhabited Time.Stream := Inhabited.mk Time.all
+
+def Time.Stream.add (s : Stream) (offset : Time) : Stream' {t : Time // t ≥ offset}
+   := fun n => ⟨offset + s n, Nat.le_add_right offset (s n)⟩
+
+abbrev NoProof := False -> Prop
+instance NoProof.inhabited : Inhabited NoProof := Inhabited.mk (
+   by
+   intro f
+   contradiction
+)
 
 structure Process where
    id : Nat
