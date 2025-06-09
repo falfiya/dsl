ced77f30b4666afa814260dc9dbd638da82e436d
falfiya <22552895+falfiya@users.noreply.github.com>
2025-06-08 18:08:06 -0700

    OWWWW MY BRAIN!!!!
    mmmhmhmhm~

--- dsl/Links.lean
+++ dsl/Links.lean
@@ -24,16 +24,16 @@ structure FairLossLink where
       -> (sendAt : Time)
       -> Stream' (Option {d : FairLossLink.Deliver // d.src = src ∧ d.dest = dest ∧ d.msg = msg ∧ d.sentAt ≥ sendAt})
       -> Stream' {d : FairLossLink.Deliver // d.src = src ∧ d.dest = dest ∧ d.msg = msg ∧ d.sentAt ≥ sendAt}
+   -- This one is obvious and follows from fairLoss
    eventualDelivery : {Message : Type}
       -> (src : Process)
       -> (dest : Process)
       -> (msg : Message)
       -> (sendAt : Time)
       -> Stream' (Option {d : FairLossLink.Deliver // d.src = src ∧ d.dest = dest ∧ d.msg = msg ∧ d.sentAt ≥ sendAt})
-      -> ∃t, send src dest msg t = some ⟨⟨src, dest, msg, t⟩, by rfl⟩
+      -> ∃t, t ≥ sendAt ∧ send src dest msg t = some ⟨⟨src, dest, msg, t⟩, by rfl⟩
    finiteDuplication : NoProof
    noCreation : NoProof
-
 
 structure StubbornLink.Deliver {Message : Type} where
    src : Process
@@ -52,24 +52,33 @@ structure StubbornLink where
       -> Stream' {d : StubbornLink.Deliver // d.src = src ∧ d.dest = dest ∧ d.msg = msg ∧ d.sentAt ≥ sendAt}
    noCreation : NoProof
 
+-- This is defined after the StubbornLink because it's the StubbornLink's job
+-- to prove this.
+def FairLossLink.infiniteSend
+   (fll : FairLossLink) (src : Process) (dest : Process) (msg : Message) (sendAt : Time)
+   : Stream' (Option {d : FairLossLink.Deliver // d.src = src ∧ d.dest = dest ∧ d.msg = msg ∧ d.sentAt ≥ sendAt})
+   := (Time.all.add sendAt).map (by
+      intro ⟨sendAt', p_sendAt'⟩
+      exact (fll.send src dest msg sendAt').map (by
+         intro ⟨delivery, p_delivery⟩
+         constructor
+         case val => exact delivery
+         simp
+         refine ⟨?_, ?_, ?_, ?_⟩
+         · rw [p_delivery]
+         · rw [p_delivery]
+         · rw [p_delivery]
+         · rw [p_delivery]; simp; exact p_sendAt'
+      ))
+
 def StubbornLink.fromFLL (fll : FairLossLink) : StubbornLink := {
    fll := fll,
    send
       {Message : Type} (src : Process) (dest : Process) (msg : Message) (sendAt : Time)
       : Stream' {d : StubbornLink.Deliver // d.src = src ∧ d.dest = dest ∧ d.msg = msg ∧ d.sentAt ≥ sendAt}
       := by
-         let maybeDelivered : Stream' (Option {d : FairLossLink.Deliver // d.src = src ∧ d.dest = dest ∧ d.msg = msg ∧ d.sentAt ≥ sendAt}) := (Time.all.add sendAt).map (fun ⟨sendAt', p_sendAt'⟩ => (fll.send src dest msg sendAt').map (by
-            intro ⟨delivery, p_delivery⟩
-            constructor
-            case val => exact delivery
-            simp
-            refine ⟨?_, ?_, ?_, ?_⟩
-            · rw [p_delivery]
-            · rw [p_delivery]
-            · rw [p_delivery]
-            · rw [p_delivery]; simp; exact p_sendAt'
-         ))
-         let fairLoss := fll.fairLoss src dest msg sendAt maybeDelivered;
+         let infinteSends := fll.infiniteSend src dest msg sendAt
+         let fairLoss := fll.fairLoss src dest msg sendAt infinteSends
          exact fairLoss.map (by
             intro ⟨fld, ⟨p_fldSrc, p_fldDest, p_fldMsg, p_fldSentAt⟩⟩
             constructor
