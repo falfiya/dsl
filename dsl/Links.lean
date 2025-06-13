145adc6aaed646ddf68875458c8efdf6ebdc3e21
falfiya <22552895+falfiya@users.noreply.github.com>
2025-06-12 18:35:45 -0700

    so close I can almost taste it

--- dsl/Links.lean
+++ dsl/Links.lean
@@ -4,125 +4,87 @@ namespace Synchronous
 
 variable {𝓜 : Type}
 
-namespace FairLoss
-   inductive Event where
+namespace FairLossLink
+   inductive Request where
       | nothing
-      | send (src : Process) (dest : Process) (msg : 𝓜)
-      | deliver (src : Process) (dest : Process) (msg : 𝓜)
-end FairLoss
+      | send (src dest : Process) (msg : 𝓜)
+   def Requests := Stream' <| @Request 𝓜
+   inductive Indication where
+      | nothing
+      | deliver (src dest : Process) (msg : 𝓜)
+   structure EventStream (reqs : Requests) where
+      inds : Stream' <| @Indication 𝓜
+      selfSend : {_ : Unit}
+         -> (p : Process)
+         -> (msg : 𝓜)
+         -> (∃ t, reqs t = .send p p msg)
+         -> (∃ t' > t, inds t' = .deliver p p msg)
+      fairLoss : {_ : Unit}
+         -> (src dest : Process)
+         -> (msg : 𝓜)
+         -> (startAt : Time)
+         -> (∀ T ≥ startAt, ∃ t ≥ T, reqs t = .send src dest msg)
+         -> (∀ T ≥ startAt, ∃ t ≥ T, inds t = .deliver src dest msg)
+      finiteDuplication : {_ : Unit}
+         -> (src dest : Process)
+         -> (msg : 𝓜)
+         -> (∃ T1, ∀ t < T1, reqs t ≠ .send src dest msg)
+         -> (∃ T2, ∀ t < T2, inds t ≠ .deliver src dest msg)
+      noCreation : {_ : Unit}
+         -> (src dest : Process)
+         -> (msg : 𝓜)
+         -> (deliveredAt : Time)
+         -> (inds deliveredAt = .deliver src dest msg)
+         -> (∃ sentAt < deliveredAt, reqs sentAt = .send src dest msg)
+      -- causality : {_ : Unit}
+      --    -> (src dest : Process)
+      --    -> (msg : 𝓜)
+      --    -> ()
+end FairLossLink
+structure FairLossLink where
+   request : (reqs : @FairLossLink.Requests 𝓜)
+      -> FairLossLink.EventStream reqs
 
-structure FairLoss where
-   es : Stream' <| @FairLoss.Event 𝓜
-   selfSend : {_ : Unit}
-      -> (p : Process)
-      -> (msg : 𝓜)
-      -> (∃ t, es t = .send p p msg)
-      -> (∃ t' > t, es t' = .deliver p p msg)
-   fairLoss : {_ : Unit}
-      -> (src : Process)
-      -> (dest : Process)
-      -> (msg : 𝓜)
-      -> (startAt : Time)
-      -> (∀ T ≥ startAt, ∃ t ≥ T, es t = .send src dest msg)
-      -> (∀ T ≥ startAt, ∃ t ≥ T, es t = .deliver src dest msg)
-   finiteDuplication : {_ : Unit}
-      -> (src dest : Process)
-      -> (msg : 𝓜)
-      -> (∃ T1, ∀ t < T1, es t ≠ .send src dest msg)
-      -> (∃ T2, ∀ t < T2, es t ≠ .deliver src dest msg)
-   noCreation : {_ : Unit}
-      -> (src dest : Process)
-      -> (msg : 𝓜)
-      -> (t : Time)
-      -> (es t = .deliver src dest msg)
-      -> (∃ t' < t, es t = .send src dest msg)
-
-structure FairLossEventStream where
+namespace StubbornLink
+   inductive Request where
+      | nothing
+      | send (src dest : Process) (msg : 𝓜)
+   def Requests := Stream' <| @Request 𝓜
+   inductive Indication where
+      | nothing
+      | deliver (src dest : Process) (msg : 𝓜)
+   def Indications := Stream' <| @Indication 𝓜
+   structure EventStream (reqs : Requests) where
+      inds : Indications
+      stubbornDelivery : {_ : Unit}
+         -> (src dest : Process)
+         -> (msg : 𝓜)
+         -> (sentAt : Time)
+         -> (reqs sentAt = .send src dest msg)
+         -> (∀ T ≥ sentAt, ∃ t ≥ T, inds t = .deliver src dest msg)
+      noCreation : {_ : Unit}
+         -> (src dest : Process)
+         -> (msg : 𝓜)
+         -> (deliveredAt : Time)
+         -> (inds deliveredAt = .deliver src dest msg)
+         -> (∃ sentAt < deliveredAt, reqs sentAt = .send src dest msg)
+end StubbornLink
+structure StubbornLink where
+   request : (reqs : @StubbornLink.Requests 𝓜)
+      -> StubbornLink.EventStream reqs
+-- def StubbornLink._request (fll : @FairLossLink 𝓜) (reqs : @StubbornLink.Requests) :=
    
 
-structure FairLossLink.Deliver where
-   src : Process
-   dest : Process
-   msg : 𝓜
-   sentAt : Time
+def StubbornLink.fromFLL (fll : @FairLossLink 𝓜) : @StubbornLink 𝓜 := {
+   request reqs : EventStream reqs := {
+      inds :=
+         let rec inds : @Indications 𝓜
+         | 0 => .nothing
+         | (n + 1) => inds n
+         inds
+   }
+} where indsHandler := 
 
-def FairLossLink.TryDeliver (pred : @Deliver 𝓜 -> Prop) : Type
-   := Option {d // pred d}
-
-structure FairLossLink.TryDeliverStream (pred : @Deliver 𝓜 -> Prop) where
-   Td := TryDeliver pred
-   stream : Stream' <| Td
-   uniqueTimes :
-      ∀ n, ¬∃ m, n != m ∧ 
-      -> 
-
-structure FairLossLink where
-   send : {_ : Unit}
-      -> (src : Process)
-      -> (dest : Process)
-      -> (msg : 𝓜)
-      -> (sendAt : Time)
-      -> FairLossLink.TryDeliver (· = ⟨src, dest, msg, sendAt⟩)
-   fairLoss : {Message : Type}
-      -> (src : Process)
-      -> (dest : Process)
-      -> (msg : Message)
-      -> (sendAt : Time)
-      -- Add proof that all of the options have different times
-      -> Stream' (Option {d : FairLossLink.Deliver // d.src = src ∧ d.dest = dest ∧ d.msg = msg ∧ d.sentAt ≥ sendAt})
-      -> Stream' {d : FairLossLink.Deliver // d.src = src ∧ d.dest = dest ∧ d.msg = msg ∧ d.sentAt ≥ sendAt}
-   -- This one is obvious and follows from fairLoss
-   eventualDelivery : {Message : Type}
-      -> (src : Process)
-      -> (dest : Process)
-      -> (msg : Message)
-      -> (sendAt : Time)
-      -> Stream' (Option {d : FairLossLink.Deliver // d.src = src ∧ d.dest = dest ∧ d.msg = msg ∧ d.sentAt ≥ sendAt})
-      -> ∃t, t ≥ sendAt ∧ send src dest msg t = some ⟨⟨src, dest, msg, t⟩, by rfl⟩
-   finiteDuplication : NoProof
-   noCreation : NoProof
-
-structure StubbornLink.Deliver {Message : Type} where
-   src : Process
-   dest : Process
-   msg : Message
-   sentAt : Time
-
-structure StubbornLink where
-   fll : FairLossLink
-   -- This is also stubborn delivery
-   send : {Message : Type}
-      -> (src : Process)
-      -> (dest : Process)
-      -> (msg : Message)
-      -> (sendAt : Time)
-      -> Stream' {d : StubbornLink.Deliver // d.src = src ∧ d.dest = dest ∧ d.msg = msg ∧ d.sentAt ≥ sendAt}
-   noCreation : NoProof
-
--- This is defined after the StubbornLink because it's the StubbornLink's job
--- to prove this.
-def FairLossLink.infiniteSend
-   (fll : FairLossLink) (src : Process) (dest : Process) (msg : Message) (sendAt : Time)
-   : Stream' (Option {d : FairLossLink.Deliver // d.src = src ∧ d.dest = dest ∧ d.msg = msg ∧ d.sentAt ≥ sendAt})
-   :=
-      (Time.all.add sendAt).map fun ⟨sendAt', p_sendAt'⟩ =>
-         (fll.send src dest msg sendAt').map (by
-            intro ⟨delivery, p_delivery⟩
-            constructor
-            case val => exact delivery
-            refine ⟨?_, ?_, ?_, ?_⟩
-            · rw [p_delivery]
-            · rw [p_delivery]
-            · rw [p_delivery]
-            · {
-               rw [p_delivery]
-               simp
-               exact p_sendAt'
-            }
-         )
-
-def StubbornLink.fromFLL (fll : FairLossLink) : StubbornLink := {
-   fll := fll,
    send
       {Message : Type} (src : Process) (dest : Process) (msg : Message) (sendAt : Time)
       : Stream' {d : StubbornLink.Deliver // d.src = src ∧ d.dest = dest ∧ d.msg = msg ∧ d.sentAt ≥ sendAt}
@@ -139,5 +101,29 @@ def StubbornLink.fromFLL (fll : FairLossLink) : StubbornLink := {
          )
    noCreation := default
 }
+
+-- This is defined after the StubbornLink because it's the StubbornLink's job
+-- to prove this.
+-- def FairLossLink.infiniteSend
+--    (fll : FairLossLink) (src : Process) (dest : Process) (msg : Message) (sendAt : Time)
+--    : Stream' (Option {d : FairLossLink.Deliver // d.src = src ∧ d.dest = dest ∧ d.msg = msg ∧ d.sentAt ≥ sendAt})
+--    :=
+--       (Time.all.add sendAt).map fun ⟨sendAt', p_sendAt'⟩ =>
+--          (fll.send src dest msg sendAt').map (by
+--             intro ⟨delivery, p_delivery⟩
+--             constructor
+--             case val => exact delivery
+--             refine ⟨?_, ?_, ?_, ?_⟩
+--             · rw [p_delivery]
+--             · rw [p_delivery]
+--             · rw [p_delivery]
+--             · {
+--                rw [p_delivery]
+--                simp
+--                exact p_sendAt'
+--             }
+--          )
+
+
 
 end Synchronous
